3e31d137c847af3e7a42a125013d06f7c2ed414c
kshittiz <kshittizchaudhary950@gmail.com>
2021-06-12 14:37:09 +0545

    Convert is added

---
+++ Unit_Converter/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Unit_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Fbtn = new System.Windows.Forms.Button();
+            this.Celsiusbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.ForeColor = System.Drawing.Color.Black;
+            this.input.Location = new System.Drawing.Point(183, 49);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(71, 22);
+            this.input.TabIndex = 1;
+            // 
+            // Output
+            // 
+            this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output.ForeColor = System.Drawing.Color.Black;
+            this.Output.Location = new System.Drawing.Point(183, 99);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(71, 22);
+            this.Output.TabIndex = 5;
+            // 
+            // Fbtn
+            // 
+            this.Fbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fbtn.ForeColor = System.Drawing.Color.Black;
+            this.Fbtn.Location = new System.Drawing.Point(15, 160);
+            this.Fbtn.Name = "Fbtn";
+            this.Fbtn.Size = new System.Drawing.Size(110, 23);
+            this.Fbtn.TabIndex = 2;
+            this.Fbtn.Text = "Fahrenheit";
+            this.Fbtn.UseVisualStyleBackColor = true;
+            this.Fbtn.Click += new System.EventHandler(this.Fbtn_Click);
+            // 
+            // Celsiusbtn
+            // 
+            this.Celsiusbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Celsiusbtn.Location = new System.Drawing.Point(179, 160);
+            this.Celsiusbtn.Name = "Celsiusbtn";
+            this.Celsiusbtn.Size = new System.Drawing.Size(75, 23);
+            this.Celsiusbtn.TabIndex = 3;
+            this.Celsiusbtn.Text = "Celsius";
+            this.Celsiusbtn.UseVisualStyleBackColor = true;
+            this.Celsiusbtn.Click += new System.EventHandler(this.Celsiusbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number to be convert";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Value Equals to";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(279, 230);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Celsiusbtn);
+            this.Controls.Add(this.Fbtn);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button Fbtn;
+        private System.Windows.Forms.Button Celsiusbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Unit_Converter/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Fbtn_Click(object sender, EventArgs e)
+        {
+            double num;
+        
+            double f;
+         
+            if(input.Text!="")
+            {
+                num = Convert.ToDouble( input.Text);
+
+                f = (1.8 * num) + 32;
+
+                Output.Text =Convert.ToString(System.Math.Round(f,2));
+
+                //  MessageBox.Show(f.ToString());
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter number which to want to convert");
+            }
+        }
+
+        private void Celsiusbtn_Click(object sender, EventArgs e)
+        {
+            double num2;
+            double c;
+
+            if(input.Text!="")
+            {
+                num2 =Convert.ToDouble( input.Text);
+
+                c = (num2 - 32) * 5 / 9;
+                Output.Text =Convert.ToString( System.Math.Round(c,2));
+
+                Console.ReadLine();
+
+            
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter the number");
+            }
+
+        }
+    }
+}
